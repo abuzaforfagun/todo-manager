b37b41e03df719231d023c58df7625f30d6bd7dd
Fagun <mdabuzaforfagun@gmail.com>
2024-07-11 20:12:35 +0600

    clean up and restructure

--- Console/main.go
+++ Console/main.go
@@ -2,35 +2,24 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
+	"todo-console/core"
+	"todo-console/store_in_file"
 )
 
-const fileName string = "tasks.txt"
-
-var taskList []Task
+type Task = core.Task
 
 func main() {
-	defer func() {
-		err := storeTasksInTextFile()
 
-		if err != nil {
-			fmt.Println("Failed to store in the file", err)
-		}
-	}()
-	var readExistingTaskErr error
-	taskList, readExistingTaskErr = readExistingTasks()
-
-	if readExistingTaskErr != nil {
-		fmt.Println("Unable to retrieve tasks", readExistingTaskErr)
+	err := store_in_file.Init()
+	if err != nil {
+		fmt.Println("Unable to initialize")
 	}
-
 	for {
 		displayMenu()
+
 		var input int32
 
 		reader := bufio.NewReader(os.Stdin)
@@ -42,77 +31,63 @@ func main() {
 
 		switch input {
 		case 1:
-			fmt.Println("Please enter the task name")
-			taskName, err := reader.ReadString('\n')
-
-			if err != nil {
-				fmt.Println("Error reading input", err)
-			}
-			addTask(taskName)
+			addTask()
 		case 2:
-			fmt.Println("Please enter the task number: ")
-			taskToDelete, err := reader.ReadString('\n')
-			if err != nil {
-				fmt.Println("Faild to retrive task number")
-			}
-			if err == nil {
-				err := deleteTask(taskToDelete)
-
-				if err != nil {
-					fmt.Println("Failed to delete the task, ", err)
-				}
-			}
-
+			deleteTask()
 		case 3:
 			displayTaskList()
 		case 4:
+			defer store_in_file.CloseConnection()
 			fmt.Println("----- THANK YOU -----")
 			return
 		default:
 			fmt.Println("Please enter correct value")
 		}
 	}
+
 }
 
-func deleteTask(taskToDelete string) error {
-	taskToDelete = strings.TrimRight(taskToDelete, "\n")
+func deleteTask() {
+	fmt.Println("Please enter the task number: ")
 
-	taskNumber, err := strconv.Atoi(taskToDelete)
+	reader := bufio.NewReader(os.Stdin)
+	taskToDelete, err := reader.ReadString('\n')
+
+	taskToDelete = strings.TrimSpace(taskToDelete)
 
 	if err != nil {
-		return err
+		fmt.Println("Faild to retrive task number")
 	}
 
-	for index, value := range taskList {
-		if value.Id == taskNumber {
-			taskList = append(taskList[:index], taskList[index+1:]...)
+	if err == nil {
+		err := store_in_file.DeleteTask(taskToDelete)
+
+		if err != nil {
+			fmt.Println("Failed to delete the task, ", err)
+			return
 		}
 	}
 
-	taskIndex := -1
-	for index, value := range taskList {
-		if strings.Contains(value.Name, taskToDelete) {
-			taskIndex = index
-			break
-		}
-	}
-	if taskIndex == -1 {
-		return errors.New("Task not found")
-	}
-	taskList = append(taskList[:taskIndex], taskList[taskIndex+1:]...)
+	clearScreen()
 
-	return nil
+	fmt.Println("Task deleted.")
 }
 
-func addTask(taskName string) {
-	taskName = strings.TrimRight(taskName, "\n")
-	taskId := len(taskList) + 1
-	task := Task{
-		Id:   taskId,
-		Name: taskName,
+func addTask() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Please enter the task name")
+	taskName, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading input", err)
 	}
 
-	taskList = append(taskList, task)
+	store_in_file.AddTask(taskName)
+
+	clearScreen()
+
+	fmt.Println("Task added.")
 }
 
 func displayMenu() {
@@ -124,41 +99,14 @@ func displayMenu() {
 }
 
 func displayTaskList() {
+	tasks := store_in_file.GetTasks()
+
 	fmt.Println("ID \t Name")
-	for _, task := range taskList {
+	for _, task := range tasks {
 		task.Print()
 	}
 }
 
-func storeTasksInTextFile() error {
-	jsonData, err := json.Marshal(taskList)
-
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(fileName, jsonData, 0644)
-
-	if err != nil {
-		return err
-	}
-
-	return err
-}
-
-func readExistingTasks() ([]Task, error) {
-	tasksFileText, err := os.ReadFile(fileName)
-
-	if err != nil {
-		file, err := os.Create(fileName)
-		defer file.Close()
-
-		return nil, err
-	}
-
-	err = json.Unmarshal(tasksFileText, &taskList)
-
-	if err != nil {
-		return nil, err
-	}
-	return taskList, nil
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
 }
---
+++ Console/store_in_file/store_in_file.go
@@ -0,0 +1,100 @@
+package store_in_file
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"todo-console/core"
+)
+
+const fileName string = "tasks.txt"
+
+type Task = core.Task
+
+var Tasks []Task
+
+func DeleteTask(taskToDelete string) error {
+
+	taskNumber, err := strconv.Atoi(taskToDelete)
+	if err == nil {
+		for index, value := range Tasks {
+			if value.Id == taskNumber {
+				Tasks = append(Tasks[:index], Tasks[index+1:]...)
+			}
+		}
+		return nil
+	}
+
+	taskIndex := -1
+	for index, value := range Tasks {
+		if strings.Contains(value.Name, taskToDelete) {
+			taskIndex = index
+			break
+		}
+	}
+	if taskIndex == -1 {
+		return errors.New("Task not found")
+	}
+	Tasks = append(Tasks[:taskIndex], Tasks[taskIndex+1:]...)
+
+	return nil
+}
+
+func AddTask(taskName string) {
+	taskName = strings.TrimRight(taskName, "\n")
+	taskId := len(Tasks) + 1
+	task := Task{
+		Id:   taskId,
+		Name: taskName,
+	}
+
+	Tasks = append(Tasks, task)
+}
+
+func store() error {
+	jsonData, err := json.Marshal(Tasks)
+
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(fileName, jsonData, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func Init() error {
+	tasksFileText, err := os.ReadFile(fileName)
+
+	if err != nil {
+		file, err := os.Create(fileName)
+		defer file.Close()
+
+		return err
+	}
+
+	err = json.Unmarshal(tasksFileText, &Tasks)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetTasks() []Task {
+	return Tasks
+}
+
+func CloseConnection() {
+	err := store()
+
+	if err != nil {
+		fmt.Println("Failed to store in the file", err)
+	}
+}
